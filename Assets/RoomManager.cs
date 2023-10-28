27357a98f4e061866f3e36eb1e96f64297c535c8
fdesroches <francois-julien.desroches@outlook.fr>
2023-10-28 12:30:05 +0200

    Connection to a server with PUN

--- Assets/RoomManager.cs
+++ Assets/RoomManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class RoomManager : MonoBehaviour
+public class RoomManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Connecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public override void OnConnectedToMaster()
     {
-        
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected to Server");
+
+        PhotonNetwork.JoinLobby();
     }
 }
