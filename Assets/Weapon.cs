96f1e9cc500fe514109b18a8e32600987998ef36
fdesroches <francois-julien.desroches@outlook.fr>
2023-11-04 18:46:31 +0100

    Add VFX - Effet weapon

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -11,6 +11,9 @@ public class Weapon : MonoBehaviour
 
     public float fireRate;
 
+    [Header("VFX")]
+    public GameObject hitVFX;
+
     private float nextFire;
 
     void Update()
@@ -35,6 +38,8 @@ public class Weapon : MonoBehaviour
 
         if (Physics.Raycast(ray.origin, ray.direction, out hit, 100f)) 
         {
+            PhotonNetwork.Instantiate(hitVFX.name, hit.point, Quaternion.identity);
+            
             if (hit.transform.gameObject.GetComponent<Health>())
             {
                 hit.transform.gameObject.GetComponent<PhotonView>().RPC("TakeDamage", RpcTarget.All, damage);
